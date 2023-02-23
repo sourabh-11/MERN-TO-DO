9fbfb97f789ab6a9d9704847031b63ef67f1f3f6
sourabh-11 <sourabhjangid2002@gmail.com>
2023-02-23 08:00:09 +0530

    data type

---
+++ app.js
@@ -0,0 +1,60 @@
+// data type in java script
+
+// 1.  Number
+// let l = 34;
+// let b=  23.3;
+
+
+// 2.String
+// let str = "sourabh";
+// let str2 = "enter the number";
+
+// 3.object 
+// const obj  = {fname: "sourabh",age:23,city:"jaipur"}
+
+
+
+// 4.array object
+// const car = ["thar","aulto","volvo"]
+
+
+// 5.bigInt
+// let x = BigInt("23343432435435098767890");
+
+// 6.booleans
+// let x = 4;
+// let  y= 5;
+// console.log(y==x);    //false
+
+
+let x = null;
+// console.log(x);
+// console.log(typeof(x));
+
+
+// let x = 23 + "sourabh"+23    
+// console.log(x);   
+
+// let x  = 23 + 3+ "string"      // x=26string
+// let x  =  "string" + 23 + 3 // x=string233
+// console.log(x);
+
+// let z= 222e-5;
+// console.log(z);
+
+
+
+
+
+
+
+//     object
+
+const car  = {name:'Audi',
+             color:"black",
+             model:444,
+            function:function() {
+                return this.name + " coloer of car"+this.color;
+                
+            }}
+console.log(car);
