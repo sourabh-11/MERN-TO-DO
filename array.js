93a80d704377af4ebca1e23a29a22f3bf627c483
sourabh-11 <sourabhjangid2002@gmail.com>
2023-03-19 22:23:15 +0530

    done

--- array.js
+++ array.js
@@ -19,6 +19,7 @@
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // console.log(fruits.reverse());
+// console.log(fruits.reverse());
 // const points = [40, 100, 1, 5, 25, 10];
 // points.sort(function(a, b){return a - b});
 // console.log(points);
